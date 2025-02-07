f0cba948ba794ff0f1fea0b439c1f818cdcc9fd5
Terry-Xu-666 <xutianyang8@126.com>
2025-02-06 23:22:39 -0500

    Update bindings_test.py

--- tests/python/bindings_test.py
+++ tests/python/bindings_test.py
@@ -3,7 +3,7 @@ import unittest
 
 import numpy as np
 
-import hnswlib_tgrag
+import hnswlib_noderag
 
 
 class RandomSelfTestCase(unittest.TestCase):
@@ -16,7 +16,8 @@ class RandomSelfTestCase(unittest.TestCase):
         data = np.float32(np.random.random((num_elements, dim)))
 
         # Declaring index
-        p = hnswlib_tgrag.Index(space='l2', dim=dim)  # possible options are l2, cosine or ip
+        p = hnswlib_noderag.Index(space='l2', dim=dim)  # possible options are l2, cosine or ip
+
 
         # Initiating index
         # max_elements - the maximum number of elements, should be known beforehand
@@ -52,7 +53,7 @@ class RandomSelfTestCase(unittest.TestCase):
         del p
 
         # Re-initiating, loading the index
-        p = hnswlib_tgrag.Index(space='l2', dim=dim)  # you can change the sa
+        p = hnswlib_noderag.Index(space='l2', dim=dim)  # you can change the sa
 
         print("\nLoading index from '%s'\n" % index_path)
         p.load_index(index_path)
